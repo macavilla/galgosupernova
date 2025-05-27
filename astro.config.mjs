e57b1e43a5372395e04d33144633c8dcda1799f5
macavilla <maca.villa.a@gmail.com>
2025-05-26 22:07:00 -0300

    chore: config github page deploy action

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,17 @@
 // @ts-check
 import { defineConfig } from 'astro/config'
 
-import tailwindcss from '@tailwindcss/vite';
+import tailwindcss from '@tailwindcss/vite'
 
-import react from '@astrojs/react';
+import react from '@astrojs/react'
 
 // https://astro.build/config
 export default defineConfig({
-  vite: {
-    plugins: [tailwindcss()]
-  },
+	site: 'https://macavilla.github.io',
+	base: '/galgosupernova',
+	vite: {
+		plugins: [tailwindcss()]
+	},
 
-  integrations: [react()]
+	integrations: [react()]
 })
