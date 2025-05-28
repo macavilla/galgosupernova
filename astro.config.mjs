0d7d260f7c0ff4915b1733a98a72e6c1b59c1abb
macavilla <maca.villa.a@gmail.com>
2025-05-28 00:31:49 -0300

    revert: add base to config

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,6 +8,7 @@ import react from '@astrojs/react'
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://macavilla.github.io',
+	base: '/galgosupernova',
 	vite: {
 		plugins: [tailwindcss()]
 	},
