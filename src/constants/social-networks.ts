0ded40c94977f73d3399ae4505c5334ff30f647b
macavilla <maca.villa.a@gmail.com>
2025-06-15 09:02:22 -0300

    feat(Footer): create footer, add to Layout

---
+++ src/constants/social-networks.ts
@@ -0,0 +1,22 @@
+interface SocialNetworkType {
+	name: string
+	url: string
+}
+export const socialNetworks: SocialNetworkType[] = [
+	{
+		name: 'instagram',
+		url: 'https://www.instagram.com/galgosupernova/'
+	},
+	{
+		name: 'Telegram',
+		url: 'https://t.me/galgosupernova'
+	},
+	{
+		name: 'Linktree',
+		url: 'https://linktr.ee/galgosupernova'
+	},
+	{
+		name: 'BikeCollectives Wiki',
+		url: 'https://bikecollectives.org/wiki/Galgo_Supernova'
+	}
+]
