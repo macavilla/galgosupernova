0841393f8346100b4304d0f62fcde28592833ecd
macavilla <maca.villa.a@gmail.com>
2025-06-17 00:21:52 -0300

    refactor(Footer): fix layout

---
+++ src/constants/social-media.ts
@@ -0,0 +1,22 @@
+interface SocialMediaType {
+	name: string
+	url: string
+}
+export const SOCIAL_MEDIA: SocialMediaType[] = [
+	{
+		name: 'instagram',
+		url: 'https://www.instagram.com/galgosupernova/'
+	},
+	{
+		name: 'Telegram',
+		url: 'https://t.me/galgosupernova'
+	},
+	{
+		name: 'Linktree',
+		url: 'https://linktr.ee/galgosupernova'
+	},
+	{
+		name: 'BikeCollectives Wiki',
+		url: 'https://bikecollectives.org/wiki/Galgo_Supernova'
+	}
+]
